6104590f1d001a5f000aaee8c75de76f54c9d5aa
Prayash Thapa <prayashthapa15@gmail.com>
2026-01-09 21:02:08 +0545

    feat:: basic shaders in bevy wgsl

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*, reflect::TypePath, render::render_resource::AsBindGroup, shader::ShaderRef,
+};
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+
+const SHADER_ASSET_PATH: &str = "shaders\\animate_shader.wgsl";
 fn main() {
 
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(MaterialPlugin::<CustomMaterial>::default())
         .add_plugins(EguiPlugin::default())
         .add_plugins(WorldInspectorPlugin::new())
         .add_systems(Startup, setup)
@@ -16,7 +22,8 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut custom_materials: ResMut<Assets<CustomMaterial>>
 ) {
     let cube_posiion = Transform::from_xyz(0.0, 1.0, -2.0);
     commands.spawn(
@@ -47,7 +54,7 @@ fn setup(
     commands.spawn(
         (
             Mesh3d(meshes.add(Cuboid::new(1.0,1.0, 1.0))),
-            MeshMaterial3d(materials.add(Color::WHITE)),
+            MeshMaterial3d(custom_materials.add(CustomMaterial {})),
             cube_posiion
         )
 
@@ -71,4 +78,12 @@ fn setup(
         )
 
     );
+}
+
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+struct CustomMaterial {}
+impl Material for CustomMaterial {
+    fn fragment_shader() -> ShaderRef{
+        SHADER_ASSET_PATH.into()
+    }
 }
